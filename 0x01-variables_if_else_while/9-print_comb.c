99c8e78d13cd385e601dec41544e35a8a4b6a1bb
thananga <nashthananga@gmail.com>
2022-12-13 17:07:09 -0800

    9-print_combo.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-prints all possible combos of single-digit numbers
+ * Return: (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');	
+	return (0);
+}
