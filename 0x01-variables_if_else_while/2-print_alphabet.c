670555d2a152263ed5856e859ac5a67d5a617970
thananga <nashthananga@gmail.com>
2022-12-13 16:03:29 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase, followed by newline
+ * Return: (0)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++);
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
